566046b0f3c77b230735558e59481561368ebe2f
chunc431 <chunc431@newschool.edu>
2022-11-15 09:03:19 -0500

    entry 9

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -6,7 +6,7 @@ function option1On() {
 
 let option1 = document.querySelector(".option1");
 option1.addEventListener("click", option1On);
-option1.textContent = 'Not quite';
+option1.textContent.toggle = 'Not quite';
 
 function option2On() {
   option2.classList.toggle("option2On");
@@ -14,7 +14,7 @@ function option2On() {
 
 let option2 = document.querySelector(".option2");
 option2.addEventListener("click", option2On);
-option2.textContent = 'Not quite';
+option2.textContent.toggle = 'Not quite';
 
 function option3On() {
   option3.classList.toggle("option3On");
@@ -22,7 +22,7 @@ function option3On() {
 
 let option3 = document.querySelector(".option3");
 option3.addEventListener("click", option3On);
-option3.textContent = 'Not quite';
+option3.textContent.toggle = 'Not quite';
 
 function option4On() {
   option4.classList.toggle("option4On");
@@ -30,7 +30,7 @@ function option4On() {
 
 let option4 = document.querySelector(".option4");
 option4.addEventListener("click", option4On);
-option4.textContent = 'Not quite';
+option4.textContent.toggle = 'Not quite';
 
 function option5On() {
   option5.classList.toggle("option5On");
@@ -38,4 +38,4 @@ function option5On() {
 
 let option5 = document.querySelector(".option5");
 option5.addEventListener("click", option5On);
-option1.textContent = 'Correct!';
+option1.textContent.toggle = 'Correct!';
