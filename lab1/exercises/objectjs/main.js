27145022f585ba157e64e202ba20bd1def7d5033
chunc431 <chunc431@newschool.edu>
2023-02-27 08:59:42 -0500

    media tunnel

---
+++ lab1/exercises/objectjs/main.js
@@ -0,0 +1,17 @@
+var mannequin = {
+  color: ["beige", "black", "silver", "white"],
+  gender: ["neutral"],
+  materials: ["wood", "cloth", "metal"],
+  canItStand: true, 
+ }
+
+ mannequin = myObj
+
+// OR
+
+const mannequin = {
+  color: ["beige", "black", "silver", "white"],
+  gender: ["neutral"],
+  materials: ["wood", "cloth", "metal"],
+  canItStand: true, 
+ }
