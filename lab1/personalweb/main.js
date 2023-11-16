ac04671eed7191de5742bdea459b812f18d58fea
chunc431 <chunc431@newschool.edu>
2023-11-16 01:15:38 -0500

    pf

--- lab1/personalweb/main.js
+++ lab1/personalweb/main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector(".header");
   const scrollBtn = document.getElementById("scrollBtn");
+  const homepage = document.getElementById("homepage");
+  const content = document.getElementById('content');
   const secondaryPage = document.getElementById("secondaryPage");
-  const modal = document.getElementById("modal-container");
-  const gridItems = document.querySelectorAll(".grid-item");
 
   const handleScroll = () => {
     const currentScrollTop = window.pageYOffset;
@@ -11,6 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.add("scrolling-down");
     } else {
       header.classList.remove("scrolling-down");
+    }
+
+    // calculate opacity
+    const contentHeight = homepage.offsetHeight;
+    const fadePoint = 200; // start fade at this scroll position
+    let opacity = 1 - currentScrollTop / (contentHeight - fadePoint);
+    if (opacity >= 0) {
+      content.style.opacity = opacity;
     }
   };
 
@@ -46,65 +54,4 @@ document.addEventListener("DOMContentLoaded", () => {
   scrollBtn.addEventListener("click", () => {
     scrollToElement(secondaryPage, 1200);
   });
-
-  const closeModal = () => {
-    modal.style.display = "none";
-  };
-
-  const openModal = (item) => {
-    modal.style.display = "block";
-
-    let projectDetails = `
-      <div class="project-details">
-      <div class="modal-content">
-        <div class="title">
-          ${item.title}
-        </div>
-        <div class="about">
-          ${item.about}
-        </div>
-        <div class="img">
-          <img class="modal-image" src="${item.image}"/>
-        </div>
-        <a class="link" href="${item.link.url}">
-          ${item.link.text}
-        </a>
-        <div class="description">
-          ${item.description}
-        <span class="close-btn">&times;</span>
-        </div>
-        </div>`;
-
-    modal.innerHTML = '';
-    modal.insertAdjacentHTML('beforeend', projectDetails);
-
-    const closeBtn = document.querySelector(".close-btn");
-    closeBtn.addEventListener("click", closeModal);
-  };
-
-  gridItems.forEach((gridItem) => {
-    gridItem.addEventListener("click", () => {
-      openModal();
-    });
-  });
-
-  // Render items function
-  function renderItems(items) {
-    const gridItems = document.querySelectorAll(".grid-item");
-
-    gridItems.forEach((gridItem, index) => {
-      gridItem.addEventListener("click", () => {
-        openModal(items[index]);
-      });
-    });
-  }
-
-  // Fetch data from project.json
-  fetch("project.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (items) {
-      renderItems(items); // Items are loaded in their original order
-    });
 });
